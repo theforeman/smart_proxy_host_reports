0c46c81a3126eb54d8e96b3fefa8823b84606ec3
Anna Vitova <avitova@avitova.remote.csb>
2021-10-15 15:57:10 +0200

    Refactor friendly messages

---
+++ lib/smart_proxy_reports/friendly_message.rb
@@ -0,0 +1,58 @@
+class FriendlyMessage
+  def initialize(result)
+    @result = result
+    @result_tree = @result["result"]
+    @task_tree = @result["task"]
+    @module_args_tree = @result_tree.dig("invocation", "module_args")
+  end
+
+  def generate_message
+    msg = "N/A"
+    return msg if @task_tree.nil? || @task_tree["action"].nil?
+    return @result_tree["msg"] if @result["failed"]
+
+    case @task_tree["action"]
+    when "ansible.builtin.package", "package" then msg = package_message
+    when "ansible.builtin.template", "template" then msg = template_message
+    when "ansible.builtin.service", "service" then msg = service_message
+    when "ansible.builtin.group", "group" then msg = group_message
+    when "ansible.builtin.user", "user" then msg = user_message
+    when "ansible.builtin.cron", "cron" then msg = cron_message
+    when "ansible.builtin.copy", "copy" then msg = copy_message
+    when "ansible.builtin.command", "ansible.builtin.shell", "command", "shell" then msg = @result_tree["stdout_lines"]
+    end
+    msg
+  rescue StandardError => e
+    logger.debug "Unable to parse result (#{e.message}): #{@result.inspect}"
+    msg
+  end
+
+  def package_message
+    detail = @result_tree["results"] || @result_tree["msg"] || "No details"
+    "Package(s) #{@module_args_tree["name"].join(",")} are #{@module_args_tree["state"]}: #{detail}"
+  end
+
+  def template_message
+    "Render template #{@module_args_tree["_original_basename"]} to #{@result_tree["dest"]}"
+  end
+
+  def service_message
+    "Service #{@result_tree["name"]} is #{@result_tree["state"]} and enabled: #{@result_tree["enabled"]}"
+  end
+
+  def group_message
+    "User group #{@result_tree["name"]} is #{@result_tree["state"]} with gid: #{@result_tree["gid"]}"
+  end
+
+  def user_message
+    "User #{@result_tree["name"]} is #{@result_tree["state"]} with uid: #{@result_tree["uid"]}"
+  end
+
+  def cron_message
+    "Cron job: #{@module_args_tree["minute"]} #{@module_args_tree["hour"]} #{@module_args_tree["day"]} #{@module_args_tree["month"]} #{@module_args_tree["weekday"]} #{@module_args_tree["job"]} and disabled: #{@module_args_tree["disabled"]}"
+  end
+
+  def copy_message
+    "Copy #{@module_args_tree["_original_basename"]} to #{@result_tree["dest"]}"
+  end
+end
